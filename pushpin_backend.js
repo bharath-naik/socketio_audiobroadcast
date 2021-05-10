cf965a0824d7157345f2bd429d16618161c2483d
BlockC <bharath.vadithya@gmail.com>
2021-05-10 13:49:09 +0530

    removed file headers

--- pushpin_backend.js
+++ pushpin_backend.js
@@ -2,6 +2,32 @@ var express = require('express');
 const app = express();
 var fs = require('fs');
 var path = require('path');
+var millisecondDelay = 10;
+var readStream = fs.createReadStream(filepath); 
+
+app.get('/', (req, res) => {
+    res.set({
+        'Content-Type': 'audio/webm',
+        'Grip-Hold': 'stream',
+        'Grip-Channel': 'myVlogs',
+        'Access-Control-Allow-Methods': 'OPTIONS, HEAD, GET, POST, PUT, DELETE',
+        'Access-Control-Expose-Headers': 'Date',
+        'Access-Control-Allow-Credentials': true,
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Max-Age': 9000
+    });
+    setTimeout(() => {  // just send defined millisecond audio
+        res.status(200).send();
+    }, millisecondDelay);
+});
+app.listen(8000);
+console.log('streaming audio data now 8000');
+
+/* CODE CODE CODE with initial file play
+var express = require('express'); 
+const app = express();
+var fs = require('fs');
+var path = require('path');
 var filepath = path.join(__dirname, "red.webm");
 var totalMillisecondsToSend = 1000;
 var readStream = fs.createReadStream(filepath); 
@@ -25,3 +51,5 @@ app.get('/', (req, res) => {
 });
 app.listen(8000);
 console.log('streaming audio data now 8000');
+
+*/
