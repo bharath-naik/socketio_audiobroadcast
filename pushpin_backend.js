8750056c2130ea869f5a70cb15f7707a8ed8ba6d
BlockC <bharath.vadithya@gmail.com>
2021-05-10 17:54:55 +0530

    this works by sending data every 100ms

--- pushpin_backend.js
+++ pushpin_backend.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var path = require('path');
 var Throttle = require('throttle');
 http.createServer(function (req, res) {
-var filepath = path.join(__dirname, "red.webm");
+var filepath = path.join(__dirname, "test.webm");
 var chunkSizeKB=1/2; //should be exponent of 2
 totalKBtoSend=128; //should be exponent of 2
 var totalChunksToSend=Math.floor(totalKBtoSend/chunkSizeKB);
@@ -21,9 +21,6 @@ const throttle = new Throttle(bitRate/8);
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Max-Age': 0
     });
-
-    var count = 0; //get count of which chunk is ready
-
     readStream.pipe(throttle).on("data", chunk => {
         // readStream.destroy();
         res.write(chunk); // write chunk in the response
@@ -40,35 +37,3 @@ const throttle = new Throttle(bitRate/8);
     });
 }).listen(8000);
 console.log('streaming audio data now 8000');
-
-/*
-var express = require('express'); 
-const app = express();
-var fs = require('fs');
-var path = require('path');
-var filepath = path.join(__dirname, "test.webm");
-var totalMillisecondsToSend = 100;
-var readStream = fs.createReadStream(filepath); 
-
-app.get('/', (req, res) => {
-  console.log("new request received")
-    res.set({
-        'Content-Type': 'audio/webm',
-        'Grip-Hold': 'stream',
-        'Grip-Channel': 'myVlogs',
-        'Access-Control-Allow-Methods': 'OPTIONS, HEAD, GET, POST, PUT, DELETE',
-        'Access-Control-Expose-Headers': 'Date',
-        'Access-Control-Allow-Credentials': true,
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Max-Age': 0
-          });
-    readStream.pipe(res); // pipe readStream to response (via throttle). chunks will be gathered into res and sent at once
-    setTimeout(() => {  // just send defined millisecond audio
-        readStream.unpipe(res);
-        readStream.destroy();
-        res.end();
-    }, totalMillisecondsToSend);
-});
-app.listen(8000);
-console.log('streaming audio data now 8000');
-*/
